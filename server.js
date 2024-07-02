57d06c8e76bbe9ecc1fdfd75eb64619d20969ab8
ianoliveirasilva <ianoliveira.tec@gmail.com>
2024-07-02 12:34:58 -0300

    changing new key

--- server.js
+++ server.js
@@ -1,8 +1,21 @@
+require('dotenv').config(); // Carrega as variáveis de ambiente do arquivo .env
+
 const express = require('express');
 const admin = require('firebase-admin');
 const path = require('path');
 
-const serviceAccount = require('./firebase-key.json');
+const serviceAccount = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, '\n'), // Substitui \n por quebras de linha reais
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_X509_CERT_URL,
+};
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
@@ -11,7 +24,7 @@ admin.initializeApp({
 
 const db = admin.firestore();
 const app = express();
-const port = 3000;
+const port = 3001;
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
@@ -53,7 +66,9 @@ app.get('/getItems', async (req, res) => {
     const snapshot = await db.collection('items').get();
     const items = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
     res.status(200).json(items);
+    
   } catch (error) {
+    console.error('Error getting item:', error);
     res.status(500).send('Error fetching items');
   }
 });
